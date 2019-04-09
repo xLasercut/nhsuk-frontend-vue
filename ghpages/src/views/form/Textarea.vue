be295e150a961d60c0e54dd1b0865ab09161d5a9
xLasercut <tianxun.wang2004@gmail.com>
2019-04-09 10:24:23 +0100

    updated textarea

--- ghpages/src/views/form/Textarea.vue
+++ ghpages/src/views/form/Textarea.vue
@@ -1,17 +1,111 @@
 <template>
-  <div>
-    <code-block>
-      <nhs-textarea></nhs-textarea>
+  <nhs-main>
+    <code-block :code="source">
+      <nhs-row>
+        <nhs-col :span="50">
+          <nhs-textarea :hint="hint" :error="error" :label="label" v-model="text" :rows="2" name="textarea"></nhs-textarea>
+        </nhs-col>
+        <nhs-col :span="50">
+          <p>Text: {{text}}</p>
+          <nhs-button name="toggle" @click="toggleError()">Toggle Error</nhs-button>
+        </nhs-col>
+      </nhs-row>
     </code-block>
 
-    <code-block>
-      <nhs-textarea label="Text area with label"></nhs-textarea>
-    </code-block>
-
-    <code-block>
-      <nhs-textarea label="Text area with label and hint">
-        <nhs-hint-text>Some hints</nhs-hint-text>
-      </nhs-textarea>
-    </code-block>
-  </div>
+    <argument-table heading="Textarea Arguments" :rows="rows"></argument-table>
+  </nhs-main>
 </template>
+
+<script>
+  var source = `<nhs-textarea :hint="hint" :error="error" :label="label" :rows="2" v-model="text">
+  </nhs-textarea>
+  
+  export default {
+    data() {
+      return {
+        hint: {
+          text: "Hint text"
+        },
+        error: {
+          text: "Error text"
+        },
+        label: {
+          text: "Label text"
+        },
+        text: ""
+      }
+    }
+  }`
+
+  export default {
+    data() { 
+      return {
+        hint: {
+          text: "Hint text"
+        },
+        error: {
+          text: "Error text"
+        },
+        label: {
+          text: "Label text"
+        },
+        text: "",
+        source: source,
+        rows: [
+          {
+            name: "name",
+            type: "string",
+            required: "yes",
+            description: "The name of the textarea, which is submitted with the form data."
+          },
+          {
+            name: "id",
+            type: "string",
+            required: "no",
+            description: "The id of the textarea. Default: NhsTextarea_<random id>"
+          },
+          {
+            name: "rows",
+            type: "number",
+            required: "no",
+            description: "Optional number of textarea rows. Default: 5"
+          },
+          {
+            name: "label",
+            type: "object",
+            required: "no",
+            description: "Arguments for the label component (e.g. text). See label component."
+          },
+          {
+            name: "hint",
+            type: "object",
+            required: "no",
+            description: "Arguments for the hint component (e.g. text). See hint-text component."
+          },
+          {
+            name: "error",
+            type: "object",
+            required: "no",
+            description: "Arguments for the error component (e.g. text). See error-text component."
+          },
+          {
+            name: "attributes",
+            type: "object",
+            required: "no",
+            description: "Any extra HTML attributes (for example class) to add to the textarea tag."
+          }
+        ]
+      }
+    },
+    methods: {
+      toggleError() {
+        if (this.error.text) {
+          this.error.text = ""
+        }
+        else {
+          this.error.text = "Error text"
+        }
+      }
+    }
+  }
+</script>
--- src/components/form/NhsTextarea.vue
+++ src/components/form/NhsTextarea.vue
@@ -1,31 +1,79 @@
 <template>
-  <div class="nhsuk-form-group">
-    <label class="nhsuk-label" :for="itemId" v-if="label">
-      {{label}}
-    </label>
-    <slot></slot>
-    <textarea class="nhsuk-textarea" :id="itemId" :rows="rows" :disabled="disabled" @blur="$emit('blur')"></textarea>
-  </div>
+  <form-item-container :error="error" :hint="hint" :label="label" :id="itemId">
+    <template #hint="hint">
+      <slot name="hint" :props="hint.props"></slot>
+    </template>
+    <template #error="error">
+      <slot name="error" :props="error.props"></slot>
+    </template>
+    <template #label="label">
+      <slot name="label" :props="label.props"></slot>
+    </template>
+    <template #form-item="formitem">
+      <textarea
+        :class="textareaClass" :id="itemId" :rows="rows" :disabled="disabled" :name="name"
+        :aria-describedby="formitem.described" v-model="model" v-bind="attributes"
+        @blur="$emit('blur')"
+      ></textarea>
+    </template>
+  </form-item-container>
 </template>
 
 <script>
   import RandomID from '../mixins/random-id.js'
+  import AddModel from '../mixins/add-model.js'
+  import FormItemContainer from './FormItemContainer.vue'
 
   export default {
     name: "NhsTextarea",
-    mixins: [RandomID],
+    mixins: [AddModel, RandomID],
+    components: {
+      FormItemContainer
+    },
     props: {
       rows: {
-        type: String,
-        default: "5"
+        type: Number,
+        default: 5
       },
       disabled: {
         type: Boolean,
         default: false
       },
       label: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      hint: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      error: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      attributes: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      name: {
         type: String,
-        default: ""
+        required: true
+      }
+    },
+    computed: {
+      textareaClass() {
+        if (this.error.text) {
+          return "nhsuk-textarea nhsuk-textarea--error"
+        }
+        return "nhsuk-textarea"
       }
     }
   }
