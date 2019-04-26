c24d7322b6d0565af4a506f9114d3cda41e7bda9
xLasercut <tianxun.wang2004@gmail.com>
2019-04-26 16:36:48 +0100

    fixed cypress unable to start

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/nhsuk-frontend-vue/'
-        : '/',
-    outputDir: "./distghpages"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/nhsuk-frontend-vue/'
+    : '/',
+  outputDir: "./distghpages",
+  pages: {
+    index: {
+      entry: 'ghpages/src/main.js',
+      template: 'ghpages/src/public/index.html',
+      filename: 'index.html'
+    }
+  },
+  configureWebpack: {
+    plugins: [
+        new CopyWebpackPlugin([{ from: 'ghpages/public/', to: '.' }])
+    ]
+  }
 }
