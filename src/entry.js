a8a2551c6d7702a10ab8f840cdc07d3f797c23cc
xLasercut <tianxun.wang2004@gmail.com>
2019-06-26 13:43:53 +0100

    updated backstop to latest version

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,6 @@
 // Import vue components
 import '../node_modules/nhsuk-frontend/dist/nhsuk.css'
-import '../node_modules/nhsuk-frontend/dist/nhsuk.min.js'
+import '../node_modules/nhsuk-frontend/dist/nhsuk.js'
 import * as components from './components/index'
 
 // install function executed by Vue.use()
